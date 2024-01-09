1912bc63705fed89390b9877590e54d22bba17bf
Miguel_LZPF <mgcdreamer@gmail.com>
2024-01-09 19:27:56 +0100

    Refactor code trust functionality and add usage examples

--- contracts/Truster.sol
+++ contracts/Truster.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "./interfaces/ICodeTrust.sol";
+import "contracts/interfaces/ICodeTrust.sol";
 
 /**
  * @title Truster
@@ -11,4 +11,13 @@ import "./interfaces/ICodeTrust.sol";
  */
 abstract contract Truster {
   ICodeTrust internal _codeTrust;
+  /**
+   * @dev Modifier that allows only trusted code to execute a function.
+   * It checks if the specified contract is trusted code using the `_codeTrust.isTrustedCode` function.
+   * If the contract is not trusted, it reverts with an error message.
+   */
+  modifier onlyTrustedCode(address contractToCheck) {
+    require(_codeTrust.isTrustedCode(contractToCheck, address(0), 0), "Not trusted code");
+    _;
+  }
 }
---
+++ contracts/usageExample/TrustedExample.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0 <0.9.0;
+
+import "contracts/usageExample/TrusterExample.sol";
+
+/**
+ * @title TrustedExample
+ * @dev This contract demonstrates how to call a function in another contract.
+ * It imports the "TrusterExample" contract and provides a function to call the "dumbFunction" in that contract.
+ * @author Miguel Gomez Carpena
+ */
+contract TrustedExample {
+  function callContract(address contractToCall) public {
+    TrusterExample(contractToCall).dumbFunction(10);
+  }
+}
